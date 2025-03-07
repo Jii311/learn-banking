160b6aecd2632c5e4ff9289e40bfea9c6cfd03e7
Jii311 <jihadfadhilahakbar@gmail.com>
2025-03-07 10:39:09 +0700

    style [JII] sidebar nav and mobile nav

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,27 @@
+import MobileNav from "@/components/MobileNav";
+import SideBar from "@/components/SideBar";
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-<main>
-    SideBar
-    {children}
-</main>
+  const loggedIn = { firstName: "John", lastName: "Doe" };
 
+  return (
+    <main className="flex h-screen w-full font-inter">
+      <SideBar user={loggedIn} />
+
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="Logo" />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
+    </main>
   );
 }
